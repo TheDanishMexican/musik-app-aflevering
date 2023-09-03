f99f69f1e88174fd8cdc7cb20013674e4a36cf8c
TheDanishMexican <danieljchristjansen@gmail.com>
2023-09-03 14:43:38 +0200

    setting up server

--- back-end-folder/data.js
+++ back-end-folder/data.js
@@ -1,3 +1,4 @@
+export const artists = 
 [
   {
     "name": "Taylor Swift",
--- back-end-folder/server.js
+++ back-end-folder/server.js
@@ -1,3 +1,17 @@
-const express = require('express');
+import express from "express";
+import fs from "fs";
+import { artists } from "./data.json";
 const app = express();
+app.use(express.json());
 const port = 3000;
+
+
+app.use(express.json());
+
+app.get('/artists/data', (req, res) => {
+    res.json(artists);
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
